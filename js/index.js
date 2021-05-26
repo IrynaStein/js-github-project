d185c3e66c35813fe8a0d870b69105251ff6ac8f
Iryna Stein <isophiestein@gmail.com>
2021-05-26 13:20:45 -0600

    Completed Repo Challenge

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // document.addEventListener("DOMContentLoaded")
 
-
+let user = {}
 const form = document.forms[0]
 
 form.addEventListener('submit', userSubmit)
@@ -22,10 +22,49 @@ function fetchUserData(p1) {
     }
     fetch(p1, confObj)
         .then(resp => resp.json())
-        .then(json => console.log({ //this needs to return an object with different key:value pairs
-            avatar: json.items[0].avatar_url, // pic
-            repo: json.items[0].repos_url,//repos to use in a link further
-            id: json.items[0].id
-        }))
+        .then(data => renderUserInfo(objFunction(data)))
 }
 
+const userList = document.querySelector('#user-list')
+const reposList = document.querySelector('#repos-list')
+
+function renderUserInfo(anydata){
+    userList.innerHTML = ""
+    reposList.innerHTML = ""
+const userLi = document.createElement('li')
+userLi.innerHTML = `${search.value}`
+userList.appendChild(userLi)
+const avatarImg = document.createElement('img')
+avatarImg.src = user.avatar
+avatarImg.setAttribute('width', "20%")
+userList.appendChild(avatarImg)
+const repos = document.createElement('h4')
+repos.id = "repos"
+repos.innerText = `View ${search.value}'s repos`
+userList.appendChild(repos)
+repos.addEventListener('click', fetchRepos)
+}
+
+function objFunction(anydata){
+    const {avatar_url, repos_url, url} = anydata.items[0]
+    user.avatar = avatar_url
+    user.repos = repos_url
+    user.url = url
+}
+
+function fetchRepos(){
+    fetch (user.repos)
+    .then (resp => resp.json())
+    .then (data2 => renderFetchedRepos(data2))//array of objects
+}
+
+function renderFetchedRepos(data2){
+data2.map(function(repoObject){
+const a = document.createElement('a')
+a.innerText = repoObject.name.toUpperCase()
+a.href = repoObject.html_url
+reposList.appendChild(a)
+const linebreak = document.createElement('br')
+reposList.appendChild(linebreak)
+})
+}
